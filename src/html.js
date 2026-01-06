49085e6933b410e36432e5a8c3cde49a463c6128
Yuan Chiu <chyuaner@gmail.com>
2026-01-06 18:24:38 +0800

    footer

--- src/html.js
+++ src/html.js
@@ -836,12 +836,14 @@ https://github.com/pure-css/pure/blob/master/LICENSE
                         }
                     })()}
                     </div>
+                    <p>本網頁可在無JS的環境運行</p>
                 </div>
                 <div class="col-4 right">
-                    <p>本專案是由 Yuan Chiu 製作</p>
+                    <p>© 2026 由 <a href="https://yuaner.tw" target="_blank">Yuan Chiu</a> 製作</p>
+                    <p>採用 <a href="https://github.com/chyuaner/cloudflare-echo-server/blob/master/LICENSE" target="_blank">MIT License</a> 授權</p>
                     <p><a href="https://github.com/chyuaner/cloudflare-echo-server" target="_blank">
-                        View on Github
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" /></svg>
+                        View on Github
                     </a></p>
                 </div>
             </div>
